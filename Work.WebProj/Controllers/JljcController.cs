82b597f0fa0190b44e23fd590ab9ef32a0f186dc
TinWu <yyuuix@gmail.com>
2016-05-06 17:32:15 +0800

    2016青創論壇靜態

---
+++ Work.WebProj/Controllers/JljcController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotWeb.WebApp.Controllers
+{
+    public class JljcController : WebFrontController
+    {
+        // GET: Event
+        public ActionResult jc2016()
+        {
+            return View();
+        }
+    }
+}
